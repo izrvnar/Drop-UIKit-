bbf7334450aab41223d6f48a680f505abb7ca74a
Ivan Zrvnar <izrvnar@gmail,com>
2022-05-22 22:08:58 -0400

    added 2 buttons to the custom cell, one for sending the user to the link provided, the other adding it to their closet

---
+++ IvanZrvnarFinalProject/Data Model/CoreDataStack.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataStack.swift
+//  IvanZrvnarFinalProject
+//
+//  Created by Ivan Zrvnar on 2022-05-22.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataStack{
+    private let modelName: String
+    
+    init(modelName: String){
+        self.modelName = modelName
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.persistentContainer.viewContext
+    }()
+    
+    
+    func saveContext () {
+        guard managedContext.hasChanges else { return }
+        
+        do {
+            try managedContext.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+    }
+    
+}
